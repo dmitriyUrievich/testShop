f2dcc1e3c7d0c7d3e1b9f7662f9e4157aa5ce9f7
dmitriyUrievich <dmitriy.bubenin@gmail.com>
2022-12-25 18:20:34 +0300

    remuve to server

--- server/app.js
+++ server/app.js
@@ -7,17 +7,13 @@ const initDatabase = require('./startUp/initDB')
 const routes = require('./routes')
 const PORT = config.get('port') ?? 8080
 const app = express()
+const path = require("path");
 
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 app.use('/api', routes)
 
-// if(process.env.NODE_ENV === 'production'){
-//   console.log('prod')
-// } else {
-//   console.log('dev')
-// }
 async function start () {
   try{
     mongoose.connection.once('open', ()=>{
@@ -35,5 +31,11 @@ async function start () {
 
 start()
 
+if (process.env.NODE_ENV === "production") {
+  app.use("/", express.static(path.join(__dirname, "client")));
 
-
+  const indexPath = path.join(__dirname, "client", "index.html");
+  app.get("*", async (req, res) => {
+    res.sendFile(indexPath);
+  });
+}
